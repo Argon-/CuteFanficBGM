2db3baf6e1bc9597e80ea5f2a443259b204a7022
Julian <MyFakeAcc.4@googlemail.com>
2014-10-16 23:40:41 +0200

    main: mainwindow new initializes itself

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@
 #include "LoopingPlayer.h"
 
 
+
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("CuteFanficBGMProductions");
-    QCoreApplication::setOrganizationDomain("CuteFanficBGMProductions.net");
-    QCoreApplication::setApplicationName("CuteFanficBGM");
+    QApplication::setOrganizationName("CuteFanficBGMProductions");
+    QApplication::setOrganizationDomain("CuteFanficBGMProductions.net");
+    QApplication::setApplicationName("CuteFanficBGM");
+    QApplication a(argc, argv);
 
     Playlist *playlist = new Playlist();
     LoopingPlayer *player = new LoopingPlayer();
 
-    QApplication a(argc, argv);
     MainWindow w (0, playlist, player);
-    w.init();
     w.show();
 
     const int r = a.exec();
